008591df7f74b75ba29030062da2139b81050d3f
Alvin Zhu <alvinzhu33@yahoo.com>
2017-02-10 15:23:57 -0500

    Getting that last bit :)

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ def draw_line( screen, x0, y0, x1, y1, color ):
     c = -b*b
     d = 2*a + b
 
-    while x0<x1:
+    while x0<=x1:
         plot(screen, color, x0, y0)
         if d>0:
             y0+=1
--- main.py
+++ main.py
@@ -6,5 +6,4 @@ color = [ 0, 255, 0 ]
 
 draw_line(screen, 0, 0, 100, 100, color)
 display(screen)
-
 save_extension(screen, 'img.png')
