ed541975ede9628645a58e4e65e5492385abb182
Alvin Zhu <alvinzhu33@yahoo.com>
2017-02-10 15:27:57 -0500

    Extra

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(screen, 0, 0, 100, 100, color)
+for y in range(0,500,5):
+    draw_line(screen, 0, 0, 500, y, color)
 display(screen)
 save_extension(screen, 'img.png')
