bda4108d140667ea6f17e03df89e8842a9fe9977
Alvin Zhu <alvinzhu33@yahoo.com>
2017-02-14 15:32:12 -0500

    Added

--- draw.py
+++ draw.py
@@ -1,39 +1,116 @@
 from display import *
 
-def draw_lineQ1( screen, x0, y0, x1, y1, color ):
-    a = y1 - y0
-    b = -(x1 - x0)
-    c = -b*b
+def draw_line( screen, x0, y0, x1, y1, color ):
+    if(x0<x1):
+        a = y1 - y0
+        b = -(x1 - x0)
+        c = -b*b
 
-    if(a<-b and a>0 and b<0):
-        d = 2*a + b
+        #Oct 1
+        if(a<-b and a>0 and b<0):
+            d = 2*a + b
 
-        while x0<=x1:
-            plot(screen, color, x0, y0)
-            if d>0:
+            while x0<x1:
+                plot(screen, color, x0, y0)
+                if d>0:
+                    y0+=1
+                    d+=2*b
+
+                x0+=1
+                d+=2*a
+
+        #Oct 2
+        if(a>-b and a>0 and b<0):
+            d = 2*b + a
+
+            while y0<y1:
+                plot(screen, color, x0, y0)
+                if d<0:
+                    x0+=1
+                    d+=2*a
+
                 y0+=1
                 d+=2*b
+
+        #Oct 7
+        if(a>b and a<0 and b<0):
+            d = -2*b + a
+
+            while y0>y1:
+                plot(screen, color, x0, y0)
+                if d<0:
+                    x0+=1
+                    d+=2*a
+
+                y0-=1
+                d-=2*b
+
+        #Oct 8
+        if(a<b and a<0 and b<0):
+            d = 2*a - b
+
+            while x0<x1:
+                plot(screen, color, x0, y0)
+                if d<0:
+                    y0-=1
+                    d-=2*b
+                
             x0+=1
             d+=2*a
 
-    if(a>-b and a>0 and b<0):
-        d = 2*b + a
+    else:
+        a = y0 - y1
+        b = -(x0 - x1)
+        c = -b*b
 
-        while x0<=x1:
-            plot(screen, color, x0, y0)
-            if d<0:
-                y0+=1
+        #Oct 5
+        if(a<-b and a>0 and b<0):
+            d = 2*a + b
+
+            while x1<=x0:
+                plot(screen, color, x1, y1)
+                if d>0:
+                    y1+=1
+                    d+=2*b
+
+                x1+=1
                 d+=2*a
-            x0+=1
-            d+=2*b
 
-    if(a<b and a<0 and b<0):
-        d = 2*a - b
+        #Oct 6
+        if(a>-b and a>0 and b<0):
+            d = 2*b + a
 
-        while x0<=x1:
-            plot(screen, color, x0, y0)
-            if d<0:
-                y0-=1
+            while y1<=y0:
+                plot(screen, color, x1, y1)
+                if d<0:
+                    x1+=1
+                    d+=2*a
+
+                y1+=1
+                d+=2*b
+
+        #Oct 3
+        if(a>b and a<0 and b<0):
+            d = -2*b + a
+
+            while y1>=y0:
+                plot(screen, color, x1, y1)
+                if d<0:
+                    x1+=1
+                    d+=2*a
+
+                y1-=1
                 d-=2*b
-            x0+=1
+
+        #Oct 4
+        if(a<b and a<0 and b<0):
+            d = 2*a - b
+
+            while x1<=x0:
+                plot(screen, color, x1, y1)
+                if d<0:
+                    y1-=1
+                    d-=2*b
+                
+            x1+=1
             d+=2*a
--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-for y in range(0,500,5):
-    draw_line(screen, 0, 0, 500, y, color)
+#for y in range(0,500,5):
+#    draw_line(screen, 0, 0, 500, y, color)
+draw_line(screen, 0, 0, 500, 250, color)
+draw_line(screen, 0, 0, 250, 500, color)
+draw_line(screen, 0, 500, 250, 0, color)
+draw_line(screen, 0, 500, 0, 250, color)
 display(screen)
 save_extension(screen, 'img.png')
